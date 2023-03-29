73a4037c1bc4a3aaa3a9c6ef0f05f7480b566987
interu <interu@sonicgarden.jp>
2023-03-29 16:19:17 +0900

    refactor: initialize_sg_fargate_railsの改善

--- lib/sg_fargate_rails/railtie.rb
+++ lib/sg_fargate_rails/railtie.rb
@@ -3,12 +3,11 @@ require 'sg_fargate_rails/healthcheck'
 
 module SgFargateRails
   class Railtie < ::Rails::Railtie
-    initializer :initialize_sg_fargate_rails do |_app|
+    initializer :initialize_sg_fargate_rails do |app|
       unless ::Rails.env.in?(%w[development test])
-        middleware = ::Rails.configuration.middleware
 
-        middleware.insert 0, SgFargateRails::AdjustCloudfrontHeaders
-        middleware.insert 1, SgFargateRails::Healthcheck
+        app.config.middleware.insert 0, SgFargateRails::AdjustCloudfrontHeaders
+        app.config.middleware.insert 1, SgFargateRails::Healthcheck
       end
     end
   end
