9d3fce48156235b37d4c962f5f5907678e51a617
Kazuki Nishikawa <kz.nishikawa@gmail.com>
2023-06-02 17:33:24 +0900

    Revert "cloudfront の ip を trusted_proxies に追加するように"
    This reverts commit e7cdb6e10cc3864858d888e838b67e21ad736f70.

--- lib/sg_fargate_rails/rack_attack.rb
+++ lib/sg_fargate_rails/rack_attack.rb
@@ -4,26 +4,6 @@ module SgFargateRails
   class RackAttack
     class << self
       def setup
-        setup_trusted_proxies
-        setup_allow_only_from_proxy
-      end
-
-      def setup_trusted_proxies
-        Rails.application.configure do
-          response_body = Net::HTTP.get(URI('https://ip-ranges.amazonaws.com/ip-ranges.json'))
-          ip_ranges = JSON.parse(response_body)
-          cloudfront_ips = ip_ranges['prefixes']
-                             .select { |v| v['service'] == 'CLOUDFRONT' }
-                             .map { |v| IPAddr.new(v['ip_prefix']) } +
-                           ip_ranges['ipv6_prefixes']
-                             .select { |v| v['service'] == 'CLOUDFRONT' }
-                             .map { |v| IPAddr.new(v['ipv6_prefix']) }
-
-          config.action_dispatch.trusted_proxies = cloudfront_ips
-        end
-      end
-
-      def setup_allow_only_from_proxy
         Rack::Attack.blocklist('allow only from proxy') do |req|
           proxy_ip_addr = SgFargateRails.config.proxy_ip_address
           return false unless proxy_ip_addr
