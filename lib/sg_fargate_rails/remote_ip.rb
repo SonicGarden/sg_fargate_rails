72d26f25a7db175b9af24be8517749a37a4f0c83
aki <lala.akira@gmail.com>
2023-10-28 12:55:46 +0900

    fix: ip6アドレスの考慮漏れ

--- lib/sg_fargate_rails/remote_ip.rb
+++ lib/sg_fargate_rails/remote_ip.rb
@@ -4,7 +4,8 @@ module SgFargateRails
   class RemoteIp < ActionDispatch::RemoteIp
     def call(env)
       req = ActionDispatch::Request.new env
-      req.remote_ip = req.headers['HTTP_CLOUDFRONT_VIEWER_ADDRESS'] ? req.headers['HTTP_CLOUDFRONT_VIEWER_ADDRESS'].split(':').first : ActionDispatch::RemoteIp::GetIp.new(req, check_ip, proxies)
+      # NOTE: HTTP_CLOUDFRONT_VIEWER_ADDRESSヘッダには127.0.0.1:3000といったIP4アドレスや2406:2d40:3090:af00:25c1:6071:b820:8e47:3000といったIP6アドレスが入っている
+      req.remote_ip = req.headers['HTTP_CLOUDFRONT_VIEWER_ADDRESS'] ? req.headers['HTTP_CLOUDFRONT_VIEWER_ADDRESS'].remove(/:\d+$/) : ActionDispatch::RemoteIp::GetIp.new(req, check_ip, proxies)
       @app.call(req.env)
     end
   end
