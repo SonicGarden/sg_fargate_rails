fa88775bd1cf6ec8ec8756809866652b30fa8c12
interu <interu@sonicgarden.jp>
2023-10-31 14:50:24 +0900

    refactor: review指摘対応

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,6 +1,3 @@
 require 'sg_fargate_rails'
 require 'rubygems'
 require 'rspec'
-
-PROJECT_ROOT = File.expand_path(File.join(File.dirname(__FILE__), '..'))
-$LOAD_PATH << File.join(PROJECT_ROOT, 'lib')
