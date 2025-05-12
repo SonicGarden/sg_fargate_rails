235a2bfbdd48f5094ea90401fd50a9865d9ce75e
kantum75 <185953013+kantum75@users.noreply.github.com>
2025-05-12 18:11:20 +0900

    cfgen のサブコマンドを追加

--- lib/sg_fargate_rails/generator_wrapper/cfgen_generator_wrapper.rb
+++ lib/sg_fargate_rails/generator_wrapper/cfgen_generator_wrapper.rb
@@ -12,18 +12,28 @@ module SgFargateRails
     private
 
     def run_command(argv)
-      commands = if argv.empty?
-                  [
-                    %w[bundle exec cf_fargate_rails_generator generate]  
-                    # [TODO]sfgen の Copilot 以外の取り込みができたら以下をアンコメントする
-                    # %w[bundle exec rails generate cf_fargate_rails_generator],
-                    # %w[bundle exec rails cf_fargate_rails_generator:check]
-                  ]
-                else
-                  [
-                    ['bundle', 'exec', 'cf_fargate_rails_generator', *argv]
-                  ]
-                end
+      task = argv.shift
+      commands = case task
+      when nil
+        [
+          %w[bundle exec cf_fargate_rails_generator generate],
+          # [TODO]sfgen の Copilot 以外の取り込みができたら以下をアンコメントする
+          # %w[bundle exec rails generate cf_fargate_rails_generator],
+          %w[bundle exec rails cf_fargate_rails_generator:check],
+        ]
+      when 'check'
+        [
+          %w[bundle exec rails cf_fargate_rails_generator:check],
+        ]
+      when 'app-gen'
+        [
+          %w[bundle exec rails generate cg_fargate_rails_generator],
+        ]
+      else
+        [
+          ['bundle', 'exec', 'cf_fargate_rails_generator', *argv],
+        ]
+      end
 
       commands.each do |command|
         system(*command, in: :in)
