05176ddfb8a82151eb3e4b911c5ffa7335a59130
kantum75 <185953013+kantum75@users.noreply.github.com>
2025-05-21 19:31:45 +0900

    case～whenのインデントを修正、cfgen の run_command の引数を修正

--- lib/sg_fargate_rails/generator_wrapper/cfgen_generator_wrapper.rb
+++ lib/sg_fargate_rails/generator_wrapper/cfgen_generator_wrapper.rb
@@ -14,26 +14,26 @@ module SgFargateRails
     def run_command(argv)
       task = argv.shift
       commands = case task
-      when nil
-        [
-          %w[bundle exec cf_fargate_rails_generator generate],
-          # [TODO]sfgen の Copilot 以外の取り込みができたら以下をアンコメントする
-          # %w[bundle exec rails generate cf_fargate_rails_generator],
-          %w[bundle exec rails cf_fargate_rails_generator:check],
-        ]
-      when 'check'
-        [
-          %w[bundle exec rails cf_fargate_rails_generator:check],
-        ]
-      when 'app-gen'
-        [
-          %w[bundle exec rails generate cg_fargate_rails_generator],
-        ]
-      else
-        [
-          ['bundle', 'exec', 'cf_fargate_rails_generator', *argv],
-        ]
-      end
+                 when nil
+                   [
+                     %w[bundle exec cf_fargate_rails_generator generate],
+                     %w[bundle exec rails generate cf_fargate_rails_generator],
+                     %w[bundle exec rails cf_fargate_rails_generator:check],
+                   ]
+                 when 'check'
+                   [
+                     %w[bundle exec rails cf_fargate_rails_generator:check],
+                   ]
+                 when 'app-gen'
+                   [
+                     %w[bundle exec rails generate cg_fargate_rails_generator],
+                   ]
+                 else
+
+                   [
+                     ['bundle', 'exec', 'cf_fargate_rails_generator', task, *argv],
+                   ]
+                 end
 
       commands.each do |command|
         system(*command, in: :in)
