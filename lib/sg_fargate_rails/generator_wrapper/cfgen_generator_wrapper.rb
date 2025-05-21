2d8a8c726ed61040713b7e2b30b076ba3467905f
kantum75 <185953013+kantum75@users.noreply.github.com>
2025-05-21 20:09:52 +0900

    caseではなくif～elsifに変更します

--- lib/sg_fargate_rails/generator_wrapper/cfgen_generator_wrapper.rb
+++ lib/sg_fargate_rails/generator_wrapper/cfgen_generator_wrapper.rb
@@ -12,24 +12,23 @@ module SgFargateRails
     private
 
     def run_command(argv)
-      task = argv.shift
-      commands = case task
-                 when nil
+      commands = if argv.empty?
                    [
                      %w[bundle exec cf_fargate_rails_generator generate],
                      %w[bundle exec rails generate cf_fargate_rails_generator],
                      %w[bundle exec rails cf_fargate_rails_generator:check],
                    ]
-                 when 'check'
+                 elsif argv.first == 'check'
                    [
                      %w[bundle exec rails cf_fargate_rails_generator:check],
                    ]
                  else
                    # Tulio 作業として、例えば "bundle exec cfgen update staging pipeline-env" などを実行する場合に使います
                    [
-                     ['bundle', 'exec', 'cf_fargate_rails_generator', task, *argv],
+                     ['bundle', 'exec', 'cf_fargate_rails_generator', *argv],
                    ]
                  end
+
 
       commands.each do |command|
         system(*command, in: :in)
